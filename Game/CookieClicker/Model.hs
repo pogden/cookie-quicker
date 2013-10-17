5b7e6a7e141376b01894a4ed68ffec0c661351ad
Prescott Ogden <ogden@pres.co.tt>
2013-10-17 14:33:31 -0700

    Add remaining buildings

--- Game/CookieClicker/Model.hs
+++ Game/CookieClicker/Model.hs
@@ -5,34 +5,75 @@ import Control.Category
 import Data.Label
 import Prelude hiding ((.), id)
 
-data GameState = GameState { _time :: Float  -- seconds
+data GameState = GameState { _time    :: Float  -- seconds
                            , _cookies :: Float
-                           , _cursors :: Int
-                           , _grandmas :: Int
+                             -- Buildings
+                           , _cursors              :: Int
+                           , _grandmas             :: Int
+                           , _farms                :: Int
+                           , _factories            :: Int
+                           , _mines                :: Int
+                           , _shipments            :: Int
+                           , _alchemyLabs          :: Int
+                           , _portals              :: Int
+                           , _timeMachines         :: Int
+                           , _antimatterCondensers :: Int
                            } deriving (Show, Eq)
 
 data Building = Cursor
-              | Grandma deriving (Ord, Eq, Enum, Bounded, Show)
+              | Grandma
+              | Farm
+              | Factory
+              | Mine
+              | Shipment
+              | AlchemyLab
+              | Portal
+              | TimeMachine
+              | AntimatterCondenser
+                deriving (Ord, Eq, Enum, Bounded, Show)
 
 mkLabels [''GameState]
 
 data Action = Buy Building | Sell Building deriving (Ord, Eq, Show)
 
 count :: Building -> (GameState :-> Int)
-count Cursor  = cursors
-count Grandma = grandmas
+count Cursor              = cursors
+count Grandma             = grandmas
+count Farm                = farms
+count Factory             = factories
+count Mine                = mines
+count Shipment            = shipments
+count AlchemyLab          = alchemyLabs
+count Portal              = portals
+count TimeMachine         = timeMachines 
+count AntimatterCondenser = antimatterCondensers
 
 cost :: Building -> GameState -> Float
 cost building g = baseCost building * 1.15 ^ (get (count building) g)
 
 baseCost :: Building -> Float
-baseCost Cursor  =  15
-baseCost Grandma = 100 
+baseCost Cursor              =         15
+baseCost Grandma             =        100 
+baseCost Farm                =        500
+baseCost Mine                =       3000
+baseCost Shipment            =      40000
+baseCost AlchemyLab          =     200000
+baseCost Portal              =    1666666
+baseCost TimeMachine         =  123456789
+baseCost AntimatterCondenser = 3999999999
             
 cps :: GameState -> Float
 cps g = 4 --manual clicking
-      + 0.1 * (fromIntegral $ get cursors g)
-      + 0.5 * (fromIntegral $ get grandmas g)
+      +      0.1 * (fromIntegral $ get cursors              g)
+      +      0.5 * (fromIntegral $ get grandmas             g)
+      +      4   * (fromIntegral $ get farms                g)
+      +     10   * (fromIntegral $ get factories            g)
+      +     40   * (fromIntegral $ get mines                g)
+      +    100   * (fromIntegral $ get shipments            g)
+      +    400   * (fromIntegral $ get alchemyLabs          g)
+      +   6666   * (fromIntegral $ get portals              g)
+      +  98765   * (fromIntegral $ get timeMachines         g)
+      + 999999   * (fromIntegral $ get antimatterCondensers g)
 
 saveUp :: Float -> GameState -> GameState
 saveUp c g 
